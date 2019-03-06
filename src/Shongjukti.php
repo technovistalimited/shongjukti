5710bf23c369e568f2c667c1d736fd55f89c0571
Mayeenul Islam <wz.islam@gmail.com>
2019-03-06 13:14:07 +0600

    More Facade methods added

--- src/Shongjukti.php
+++ src/Shongjukti.php
@@ -14,9 +14,24 @@ use Technovistalimited\Shongjukti\App\Models\Attachment;
 
 class Shongjukti
 {
+	public static function getAttachmentTypesByScopeKey($scopeKey)
+	{
+		return AttachmentType::getAttachmentTypesByScopeKey($scopeKey, false, false, true);
+	}
+
+	public static function storeAttachments($inputs, $scopeKey = null, $scopeId = null)
+	{
+		return Attachment::storeAttachments($inputs, $scopeKey, $scopeId);
+	}
+
 	public static function getAttachments($scopeKey, $scopeId)
 	{
 		return Attachment::getAttachments($scopeKey, $scopeId);
+	}
+
+	public static function getAttachmentsForEdit($scopeKey, $scopeId)
+	{
+		return Attachment::getAttachmentsForEdit($scopeKey, $scopeId);
 	}
 
 	public static function attachmentTypeIndexLink($scopeKey = null)
