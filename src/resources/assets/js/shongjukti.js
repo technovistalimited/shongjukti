2e74cecd8da72e684da62cc550df77a2a41361ff
Mayeenul Islam <wz.islam@gmail.com>
2020-01-07 17:15:02 +0600

    (feature) Shongjukti JS Validation

--- src/resources/assets/js/shongjukti.js
+++ src/resources/assets/js/shongjukti.js
@@ -46,18 +46,23 @@
             var mime_type      = this.files[0].type;
             var accepted_types = input.attr('accept').split(',');
 
-            // Display error message
+            // Display error message:
             // if mime_type is undetectable, or detected mime_type is not in accepted mime_types array.
             if( ('' == mime_type) || ('' !== mime_type && -1 == accepted_types.indexOf(mime_type)) ) {
-                button.find('span').html('<span class="text-danger">'+ _attachments.file_type_msg +'</span>');
+                input.val(''); // Empty the file field.
+                button.find('span').html(_attachments.browse_text); // Show the 'Browse...' text.
+                button.after('<div class="shongjukti-err-file small text-danger">' + _attachments.file_type_msg + '</div>');
             }
-            // Display error message
+            // Display error message:
             // if the filesize exceeds the maximum upload size allowed.
             else if( this.files[0].size > _attachments.max_upload_size ) {
-                button.find('span').html('<span class="text-danger">'+ _attachments.file_size_msg +'</span>');
+                input.val(''); // Empty the file field.
+                button.find('span').html(_attachments.browse_text); // Show the 'Browse...' text.
+                button.after('<div class="shongjukti-err-file small text-danger">'+ _attachments.file_size_msg +'</div>');
             } else {
+                // Display the file name inside the button.
                 var custom_label = label.length > 15 ? '...' + label.slice(-15) : label;
-                button.find('span').html( custom_label );
+                button.find('span').html(custom_label);
 
                 attachment_is_deleted.val('0');
 
@@ -74,6 +79,27 @@
                 this_attachment_row.find('.attachment-label').removeAttr('required');
             }
         }
+    });
+
+    /**
+     * Bootstrap4-specific Validation.
+     *
+     * Show mandatory warning on invalid mandatory fields.
+     */
+    $('form').on('change submit', function(e) {
+        $('.attachment-file').each(function (index) {
+            var attachment_file = $(this);
+            var attachment_btn = attachment_file.parent('.btn-file');
+            if (attachment_file.is(':invalid')) {
+                attachment_btn.addClass('border-danger').removeClass('border-success');
+                attachment_btn.siblings('.badge.badge-danger').remove(); // remove previously appended badges.
+                attachment_btn.after('<span class="badge badge-danger ml-2">Required</span>');
+            } else {
+                attachment_file.parent('.btn-file').siblings('.shongjukti-err-file').remove();
+                attachment_btn.removeClass('border-danger').addClass('border-success');
+                attachment_btn.siblings('.badge.badge-danger').remove();
+            }
+        });
     });
 
 });
